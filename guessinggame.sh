d853c790d42e21595eca443a7a30b221482bc02e
pramodr31 <66017834+pramodr31@users.noreply.github.com>
2020-05-29 16:17:23 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,32 @@
+#! bin/bash
+
+####Calculating number of files using for loop####
+count=0
+for i in ./*
+do
+        count=$(($count + 1))
+done
+
+#### Function to ask user for input and verify ####
+asknumber (){
+#### echo "Guess the number of files in this Directory"
+        read -p "Guess the number of Files in this Directory : " guess
+
+####If/Else statement to verify the results####
+if  [ $guess -eq $count ]; then
+        echo "Congratulations!!!! you guessed it right"
+        echo "Thank you participating"
+
+elif [ $guess -lt $count ]; then
+        echo "Sorry, the number you guessed is lesser than expected"
+        echo "\n"
+        asknumber
+else
+        echo "Sorry, the number you guessed is greater than expected"
+        echo "\n"
+        asknumber
+fi
+}
+
+##Calling the function when program is ran##
+asknumber
