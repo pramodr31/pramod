e08223d2bdd19427cf481e6810f9d1bd7ab7c940
pramodr31 <66017834+pramodr31@users.noreply.github.com>
2020-06-01 09:20:08 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,29 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-####Calculating number of files using for loop####
-count=0
-for i in ./*
-do
-        count=$(($count + 1))
-done
+
+####Calculating number of files####
+count=$(ls -a| wc -l)
 
 #### Function to ask user for input and verify ####
 asknumber (){
 #### echo "Guess the number of files in this Directory"
         read -p "Guess the number of Files in this Directory : " guess
 
+
+##### Validating the data entered by user to make sure it is an integer #####
+        while true;
+do
+        if [[ $guess ]] && [ $guess -eq $guess 2>/dev/null ]
+        then
+        break
+        else
+        echo "Please enter numerical value"
+        read guess
+        fi
+done
+
+
 ####If/Else statement to verify the results####
-if  [ $guess -eq $count ]; then
+        if  [[ $guess -eq $count ]]
+        then
         echo "Congratulations!!!! you guessed it right"
         echo "Thank you participating"
 
-elif [ $guess -lt $count ]; then
+elif [[ $guess -lt $count ]]; then
         echo "Sorry, the number you guessed is lesser than expected"
-        echo "\n"
+        echo ""
         asknumber
 else
         echo "Sorry, the number you guessed is greater than expected"
-        echo "\n"
+        echo ""
         asknumber
 fi
 }
